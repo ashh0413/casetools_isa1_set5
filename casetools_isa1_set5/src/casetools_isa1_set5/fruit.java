fadb27ce166a07acc5d6b7d40a9a775416f16744
exam <astonajoy77@gmail.com>
2024-02-12 10:41:26 +0530

    hey

---
+++ casetools_isa1_set5/src/casetools_isa1_set5/fruit.java
@@ -0,0 +1,19 @@
+package casetools_isa1_set5;
+
+public class fruit {
+
+
+		
+	String color;
+		fruit(String color)
+		{
+			this.color=color;
+		}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			fruit cherry = new fruit("green");
+			//String color = cherry.getColor();
+			//System.out.println("color of cherry is " + color);
+	}
+
+}
